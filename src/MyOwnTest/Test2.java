7ed9bf65d87c6597ac9c1f609019432230cf1ffd
Andrey <santa-andrio@mail.ru>
2016-03-08 13:48:16 +0300

    Made level05\lesson12\home05 work

---
+++ src/MyOwnTest/Test2.java
@@ -0,0 +1,34 @@
+package MyOwnTest;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+
+/**
+ * Created by Admin on 08.03.2016.
+ */
+public class Test2
+{
+/* Вводить с клавиатуры числа и считать их сумму
+Вводить с клавиатуры числа и считать их сумму, пока пользователь не введёт слово «сумма». Вывести на экран полученную сумму.
+*/
+    public static void main(String[] args) throws Exception
+    {
+        //напишите тут ваш код
+        BufferedReader buffer = new BufferedReader(new InputStreamReader(System.in));
+        String s = buffer.readLine();
+        int sum = 0;
+        int m = 0;
+        while (!s.equals("сумма")){
+            try {
+                sum += Integer.parseInt(s);
+            } catch (NumberFormatException e) {
+                System.err.println("Неверный формат строки12");
+                System.out.println("Было " + sum);
+            }
+//             sum += m;
+            s = buffer.readLine();
+        }
+        System.out.println(sum);
+
+    }
+}
