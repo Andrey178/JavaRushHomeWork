be844eae8c34c594e2ab55cb7b47ccc6367c8b0d
Andrey <santa-andrio@mail.ru>
2016-04-10 16:50:55 +0300

    Made level10\lesson11\home09 work

---
+++ src/com/javarush/test/level10/lesson11/home09/Solution.java
@@ -0,0 +1,63 @@
+package com.javarush.test.level10.lesson11.home09;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Map;
+
+/* Одинаковые слова в списке
+Ввести с клавиатуры в список 20 слов. Нужно подсчитать количество одинаковых слов в списке.
+Результат нужно представить в виде словаря Map<String, Integer>, где первый параметр – уникальная строка,
+а второй – число, сколько раз данная строка встречалась в списке.
+Вывести содержимое словаря на экран.
+В тестах регистр (большая/маленькая буква) влияет на результат.
+*/
+
+public class Solution
+{
+    public static void main(String[] args) throws Exception
+    {
+        BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+
+        ArrayList<String> words = new ArrayList<String>();
+        for (int i = 0; i < 20; i++)
+        {
+            words.add(reader.readLine());
+//            words.add("Мама");
+//            words.add("Мыла");
+//            if (i%4 == 0) words.add("хлюгенхайм");
+//            i++;
+
+        }
+
+        Map<String, Integer> map = countWords(words);
+
+        for (Map.Entry<String, Integer> pair : map.entrySet())
+        {
+            System.out.println(pair.getKey() + " " + pair.getValue());
+        }
+    }
+
+    public static Map<String, Integer> countWords(ArrayList<String> list)
+    {
+        HashMap<String, Integer> result = new HashMap<String, Integer>();
+
+        //напишите тут ваш код
+        for (String  s: list)
+        {
+            int i;
+            if (!result.containsKey(s))
+            {
+                result.put(s, 1);
+            } else {
+                i = result.get(s);
+                result.put(s, ++i);
+            }
+        }
+
+
+        return result;
+    }
+
+}
