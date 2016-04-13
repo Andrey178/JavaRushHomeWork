3bf8d97ea9c395a7aed7400de72b9c284a134047
Andrey <santa-andrio@mail.ru>
2016-04-13 17:58:28 +0300

    Made level13\lesson11\home09 work Made level13\lesson11\home10 work

---
+++ src/com/javarush/test/level13/lesson11/home09/Solution.java
@@ -0,0 +1,46 @@
+package com.javarush.test.level13.lesson11.home09;
+
+import java.awt.*;
+
+/* Один метод в классе
+1. Унаследовать Fox от интерфейса Animal.
+2. Поменяй код так, чтобы в классе Fox был только один метод - getName.
+3. Методы удалять нельзя!
+*/
+
+public class Solution
+{
+    public static void main(String[] args) throws Exception
+    {
+    }
+
+    public interface Animal
+    {
+        Color getColor();
+
+        Integer getAge();
+    }
+
+    abstract class Fox implements Animal
+    {
+        public String getName()
+        {
+            return "Fox";
+        }
+    }
+    class BigFox extends Fox
+    {
+
+        @Override
+        public Color getColor()
+        {
+            return null;
+        }
+
+        @Override
+        public Integer getAge()
+        {
+            return null;
+        }
+    }
+}
