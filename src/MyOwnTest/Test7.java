0895fa81e058d1abab9ec326228d9fe3fe698281
Andrey <santa-andrio@mail.ru>
2016-04-14 19:57:56 +0300

    Made level14\lesson04\task02, 03, 05 web work chto-to dobavilos samo

--- src/MyOwnTest/Test7.java
+++ src/MyOwnTest/Test7.java
@@ -4,5 +4,47 @@ package MyOwnTest;
  * Created by Admin on 12.04.2016.
  */
 public class Test7
+/* Баг в initializeIdAndName
+1. Подумать, что в программе неправильно.
+2. Вынести реализацию метода initializeIdAndName в класс User.
+3. initializeIdAndName в классе User должен возвращать тип User.
+4. Поправить программу, чтобы компилировалась и работала.
+*/
+
 {
+    public static void main(String[] args) throws Exception
+    {
+        System.out.println(new Matrix().NEO);
+        System.out.println(new Matrix().TRINITY);
+    }
+
+    static class Matrix
+    {
+        public static DBObject NEO = new User().initializeIdAndName(1, "Neo");
+        public static DBObject TRINITY = new User().initializeIdAndName(2, "Trinity");
+    }
+
+    interface DBObject
+    {
+        DBObject initializeIdAndName(int id, String name);
+    }
+
+    static class User implements DBObject
+    {
+        int id;
+        String name;
+
+        public DBObject initializeIdAndName(int id, String name) {
+            this.id = id;
+            this.name = name;
+            return this;
+        }
+
+        @Override
+        public String toString()
+        {
+            return String.format("User has name %s, id = %d", name, id);
+        }
+    }
+
 }
--- src/MyOwnTest/Test8.java
+++ src/MyOwnTest/Test8.java
@@ -5,4 +5,22 @@ package MyOwnTest;
  */
 public class Test8
 {
+    public static void main(String[] args) throws Exception
+    {
+    }
+
+    interface SimpleObject<T>
+    {
+        SimpleObject<T> getInstance();
+    }
+
+    class StringObject implements SimpleObject<String>
+            //допишите здесь ваш код
+    {
+        public SimpleObject<String> getInstance()
+        {
+            String s = "";
+            return null;
+        }
+    }
 }
