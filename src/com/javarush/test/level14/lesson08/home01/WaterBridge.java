9033b6ec66b96811945b2aba8fc755550ddcaed4
Andrey <santa-andrio@mail.ru>
2016-04-14 23:21:38 +0300

    Made level14\lesson08\home01 work

---
+++ src/com/javarush/test/level14/lesson08/home01/WaterBridge.java
@@ -0,0 +1,13 @@
+package com.javarush.test.level14.lesson08.home01;
+
+/**
+ * Created by Admin on 14.04.2016.
+ */
+public class WaterBridge implements Bridge
+{
+    @Override
+    public int getCarsCount()
+    {
+        return 0;
+    }
+}
