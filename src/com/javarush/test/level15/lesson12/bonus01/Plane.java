ac5363e939f4fc3ef7eddae835669f1b67f19c49
Andrey <santa-andrio@mail.ru>
2016-04-17 09:06:19 +0300

    Made level15\lesson12\bonus01 work

---
+++ src/com/javarush/test/level15/lesson12/bonus01/Plane.java
@@ -0,0 +1,18 @@
+package com.javarush.test.level15.lesson12.bonus01;
+
+/**
+ * Created by Admin on 17.04.2016.
+ */
+public class Plane implements Flyable
+{
+    private int maxpassengers;
+
+    Plane(int maxpassengers) {
+        this.maxpassengers = maxpassengers;
+    }
+    @Override
+    public void fly()
+    {
+
+    }
+}
