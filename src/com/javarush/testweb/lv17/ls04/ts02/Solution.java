7a134f8c121195fc32be0ed1b1e69bec513a7500
Andrey <santa-andrio@mail.ru>
2016-04-20 11:59:48 +0300

    Made level17 lesson04,06 web work

---
+++ src/com/javarush/testweb/lv17/ls04/ts02/Solution.java
@@ -0,0 +1,8 @@
+package com.javarush.testweb.lv17.ls04.ts02;
+
+/**
+ * Created by Admin on 19.04.2016.
+ */
+public class Solution
+{
+}
---
+++ src/com/javarush/testweb/lv17/ls06/ts02/Solution.java
@@ -0,0 +1,8 @@
+package com.javarush.testweb.lv17.ls06.ts02;
+
+/**
+ * Created by Admin on 20.04.2016.
+ */
+public class Solution
+{
+}
